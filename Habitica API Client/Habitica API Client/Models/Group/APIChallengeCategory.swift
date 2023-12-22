777fd55b93298d362cfbf4fccc08c1d11fc65aa8
JBYoshi <12983479+JBYoshi@users.noreply.github.com>
2023-12-22 10:31:01 -0600

    Fix challenge tags displaying incorrectly.
    Challenge tag IDs are specified using the "_id" field in the REST API, but the Swift code looks
    for an "id" field since the Swift field is named "id". This causes every tag to use the nil ID,
    which means that the most recently seen tag overwrites all previous tags. This commit fixes
    that by explicitly using the "_id" field from the JSON representation.

--- Habitica API Client/Habitica API Client/Models/Group/APIChallengeCategory.swift
+++ Habitica API Client/Habitica API Client/Models/Group/APIChallengeCategory.swift
@@ -13,4 +13,17 @@ class APIChallengeCategory: ChallengeCategoryProtocol, Decodable {
     var id: String?
     var slug: String?
     var name: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case id = "_id"
+        case slug
+        case name
+    }
+    
+    public required init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        id = try? values.decode(String.self, forKey: .id)
+        name = try? values.decode(String.self, forKey: .name)
+        slug = try? values.decode(String.self, forKey: .slug)
+    }
 }
